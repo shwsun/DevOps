c3ccf38c5001d0b2d9b0741b03a830f543ec28c9
shwsun <53630710+shwsun@users.noreply.github.com>
2021-02-05 18:29:38 +0900

    Update boot_docker.sh

--- boot_docker.sh
+++ boot_docker.sh
@@ -1,8 +1,12 @@
-# -------------------------- K8s용 도커 설치 ----------------
+# (도커 CE 설치)
+## 리포지터리 설정
+### 필요한 패키지 설치
 sudo yum install -y yum-utils device-mapper-persistent-data lvm2
 
 ## 도커 리포지터리 추가
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+#sudo yum-config-manager --save --setopt=kubernetes.sslverify=false
+sudo yum repolist
 
 # 도커 CE 설치
 sudo yum update -y && sudo yum install -y containerd.io-1.2.13 docker-ce-19.03.11 docker-ce-cli-19.03.11
@@ -32,3 +36,6 @@ sudo mkdir -p /etc/systemd/system/docker.service.d
 sudo systemctl daemon-reload
 sudo systemctl restart docker
 sudo systemctl enable docker
+
+
+
